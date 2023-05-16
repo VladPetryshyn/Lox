189971d6ef6256e55a8d7d4e22a9db4950c3ed9f
VladPetryshyn <vlad_petryshyn@pm.me>
2023-05-16 18:31:15 +0200

    builded docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://VladPetryshyn.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Lox/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
