6ed04a8d01af7ed7764629082c683b057b7ecf6d
VladPetryshyn <vlad_petryshyn@pm.me>
2023-05-17 17:29:20 +0200

    removed docusaurus files

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/vlad/Documents/Documents/projects/tox2/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/vlad/Documents/Documents/projects/tox2/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/vlad/Documents/Documents/projects/tox2/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/vlad/Documents/Documents/projects/tox2/docs/src/css/custom.css'),
-];
